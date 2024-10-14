8817c617418655ece40829c138ef54dc49fef922
colepa <colealtonparis@gmail.com>
2024-10-14 10:26:05 +0100

    FinalVer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,22 +2,53 @@ using System.Collections;
 using System . Collections . Generic ;
 using UnityEngine ;
 using UnityEngine . InputSystem ;
+using TMPro;
 
 public class PlayerController1 : MonoBehaviour
 {
 
-public Vector2 moveValue;
-public float speed;
+    public Vector2 moveValue;
+    public float speed;
+    private int PickupCount;
+    private int numPickups = 3;
+    public TextMeshProUGUI ScoreText;
+    public TextMeshProUGUI WinText;
 
-void OnMove(InputValue value)
+    void Start()
+    {
+        PickupCount = 0;
+        WinText.text = "";
+        SetCountText();
+    }
+    void OnMove(InputValue value)
     {
         moveValue = value.Get<Vector2>();
         }
-void FixedUpdate()
+    void FixedUpdate()
     {
         Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
         
-GetComponent<Rigidbody>().AddForce(movement * speed * Time.
-fixedDeltaTime);
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.
+        fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "PickUp")
+        {
+            other.gameObject.SetActive(false);
+            PickupCount += 1;
+            SetCountText();
         }
+    }
+    private void SetCountText()
+    {
+        ScoreText.text = "Score: " + PickupCount.ToString();
+        if(PickupCount >= numPickups)
+        {
+            WinText.text = "You win!";
+        }
+    }
+
+    
 }
